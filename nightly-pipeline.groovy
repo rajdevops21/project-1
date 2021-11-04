74a4a7dcc707e79a918feb6369c46df84761aa1c
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-04 13:59:04 -0400

    Update nightly-pipeline.groovy

--- nightly-pipeline.groovy
+++ nightly-pipeline.groovy
@@ -2,3 +2,11 @@
 
 @Library('test-jenkins') _
 jenkinsCommon 'nightly'
+
+triggers {
+        // Requires https://plugins.jenkins.io/parameterized-scheduler/
+        parameterizedCron('''
+            # pdsw-sonos-controller-player-s2 repo
+            H 23 * * *   %jobtype=parent-nightly
+        ''')
+    }
