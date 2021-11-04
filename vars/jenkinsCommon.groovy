5f341035996eee30a9af431eb5eaff5a72dfc016
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-04 13:17:31 -0400

    Update jenkinsCommon.groovy

--- vars/jenkinsCommon.groovy
+++ vars/jenkinsCommon.groovy
@@ -7,7 +7,7 @@ def call(String jobtype) {
             string(name: 'gitremote_training', defaultValue: 'git@github.com:Sonos-Inc/pdsw-sonos-controller-player-s2-training.git', description: 'Git remote with product source code')
             string(name: 'jenkinsdslgitremote', defaultValue: 'git@github.com:Sonos-Inc/pdsw-jenkins-dsl.git', description: 'Git remote for Jenkins DSL repo')
             string(name: 'jenkinsdslgitbranch', defaultValue: 'p4/main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from')
-            //string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')
+            string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')
         }
         options {
             disableConcurrentBuilds()
