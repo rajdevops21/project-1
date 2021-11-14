7d130e6929d3edb7704b520b5755eda460128944
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-14 18:15:29 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -1,4 +1,18 @@
-node{
-def ci = load “common.groovy”
-ci.mycommoncode()
+pipeline {
+    agent any
+
+    triggers {
+        // Requires https://plugins.jenkins.io/parameterized-scheduler/
+        parameterizedCron('''
+            # pdsw-sonos-controller-player-s2 repo
+            H 23 * * *   %jobtype=parent-nightly
+        ''')
+    }
+    stages {
+        stage('Nightly-Run') {
+            steps {
+                def nightly = load "common.groovy"
+            }
+        }
+    }
 }
