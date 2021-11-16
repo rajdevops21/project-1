16031fd04e257a3d483f371f5edb2268e22c0cff
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-15 22:40:46 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -1,4 +1,8 @@
 def ci
+properties([[$class: 'BuildDiscarderProperty',
+                strategy: [$class: 'LogRotator', numToKeepStr: '10']],
+                pipelineTriggers([cron('H/30 * * * *')]),
+                ])
 node{
   parameters {
         string(name: 'gitremote', defaultValue: 'git@github.com:Sonos-Inc/pdsw-sonos-controller-player-s2.git', description: 'Git remote with product source code')
@@ -7,12 +11,6 @@ node{
         string(name: 'jenkinsdslgitbranch', defaultValue: 'p4/main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from')
         string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')
   }
-  properties(
-    [
-        ...  , // other properties that you have
-        pipelineTriggers([cron('H/10 * * * *')]),
-    ]
-  )
   ci = load 'nightly.groovy'
   ci.ciBranch()
 }
