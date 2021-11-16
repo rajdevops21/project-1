04d5d92a14bbe7a96dea2a968c4b910cdad42777
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-15 23:07:15 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -1,7 +1,7 @@
 def ci
 properties([[$class: 'BuildDiscarderProperty',
                 strategy: [$class: 'LogRotator', numToKeepStr: '10']],
-                pipelineTriggers([cron('* * * * *')]),
+                pipelineTriggers([parameterizedCron('H/10 * * * * %jobtype=ci')]),
                 ])
 node{
   parameters {
