87a19991619a3a2ca0d0e3fb7dbd57939dbdce83
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-15 20:28:25 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -1,4 +1,18 @@
 node {
+   
+       // now you are on slave labeled with 'label'
+    def workspace = WORKSPACE
+    // ${workspace} will now contain an absolute path to job workspace on slave
+
+    workspace = env.WORKSPACE
+    // ${workspace} will still contain an absolute path to job workspace on slave
+
+    // When using a GString at least later Jenkins versions could only handle the env.WORKSPACE variant:
+    echo "Current workspace is ${env.WORKSPACE}"
+
+    // the current Jenkins instances will support the short syntax, too:
+    echo "Current workspace is $WORKSPACE"
+   
    stage("Determine build file") {
       String JenkinsFile = nightly.groovy
    }
