09687901378dc1b65b34ec841670176c72f7cabd
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-15 22:14:11 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -1,6 +1,12 @@
 def ci
 node{
-  checkout scm
+  triggers {
+            // Requires https://plugins.jenkins.io/parameterized-scheduler/
+            parameterizedCron('''
+                # pdsw-sonos-controller-player-s2 repo
+                H 23 * * *   %jobtype=parent-nightly
+            ''')
+  }
   ci = load 'nightly.groovy'
   ci.ciBranch()
 }
