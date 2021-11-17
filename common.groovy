af942ec9395bf50e28ba48dbd601b117a8b1ffd5
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-17 14:52:34 -0500

    Update common.groovy

--- common.groovy
+++ common.groovy
@@ -1,17 +1,10 @@
 import jenkins.model.Jenkins
 //import hudson.slaves.EnvironmentVariablesNodeProperty
-import hudson.EnvVars
+//import hudson.EnvVars
 
 def commoncode(){
     timeout(150) {
         node {
-            properties([
-                parameters(
-                    [string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
-                     string(name: 'jenkinsdslgitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote for Jenkins DSL repo'),
-                     string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),]
-                )
-            ])
             stage('Build') {
                 print "DEBUG: parameter jobtype = ${jobtype}"
             }
