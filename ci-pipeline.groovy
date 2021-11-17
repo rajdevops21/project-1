6397a63b756ff812fb70e34175fde026634b15fd
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-17 08:20:47 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -4,7 +4,7 @@ node{
   checkout scm
   
   properties([[$class: 'BuildDiscarderProperty',
-                strategy: [$class: 'LogRotator', numToKeepStr: '10']],
+                strategy: [$class: 'LogRotator', numToKeepStr: '30']],
             pipelineTriggers([cron('H/10 * * * *')]),
             parameters(
               [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
