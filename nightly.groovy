979fad3329b5f6e4f38ba033a790f62a4d492d1b
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-16 21:13:57 -0500

    Update nightly.groovy

--- nightly.groovy
+++ nightly.groovy
@@ -10,6 +10,29 @@ def ciBranch(){
         stage('Test') {
             print "DEBUG: parameter jobtype = ${jobtype}"
         }
+        stage('Git Fetch tags') {
+             sh '''#!/bin/bash
+                    set -e -x -o pipefail
+                    mkdir gitrepo
+                    cd gitrepo
+                    git clone ${gitremote} . --no-checkout
+                    if [ "${gitremote_training}" ]; then
+                        git remote add training "${gitremote_training}"
+                    fi
+                    git fetch --all
+                    echo ========== Initial Git Refs for Source ============
+                    git show-ref
+                    cd ..
+                    mkdir jenkinsdsl
+                    cd jenkinsdsl
+                    git clone ${jenkinsdslgitremote} . --no-checkout
+                    git fetch --all --tags
+                    git checkout ${jenkinsdslgitbranch}
+                    echo ========== Git refs for Jenkins DSL ===============
+                    git show-ref
+                    cd ..
+                '''
+        }
     }
 }
 return this
