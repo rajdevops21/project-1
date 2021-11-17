9e80df72c4c8fa7a46152f45ecd1100e44344d28
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-17 14:49:31 -0500

    Update nightly-pipeline.groovy

--- nightly-pipeline.groovy
+++ nightly-pipeline.groovy
@@ -9,7 +9,7 @@ timeout(150) {
             pipelineTriggers([cron('H/10 * * * *')]),
             parameters(
               [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
-               string(name: 'jobtype', defaultValue: 'nightly', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
+               string(name: 'jobtype', defaultValue: 'nightly', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')]
             )
                 ])
     nightly = load 'common.groovy'
