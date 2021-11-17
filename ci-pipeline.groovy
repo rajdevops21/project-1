2abff92227134bb276f0a752ad4bc17e87cd1291
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-16 22:23:26 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -13,7 +13,7 @@ properties(
         string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
         string(name: 'jenkinsdslgitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote for Jenkins DSL repo'),
         string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
-        string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')]
+        string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
         )
       ]
   )
