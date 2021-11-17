95268702563641380900ad804f3f45b5b1dfad36
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-17 14:46:03 -0500

    Update nightly-pipeline.groovy

--- nightly-pipeline.groovy
+++ nightly-pipeline.groovy
@@ -9,9 +9,6 @@ timeout(150) {
             pipelineTriggers([cron('H/10 * * * *')]),
             parameters(
               [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
-               string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
-               string(name: 'jenkinsdslgitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote for Jenkins DSL repo'),
-               string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
                string(name: 'jobtype', defaultValue: 'nightly', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
             )
                 ])
