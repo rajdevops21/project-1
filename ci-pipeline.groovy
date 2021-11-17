a63c98c5f38845c70468a99a9bb317d54c13526b
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-16 22:43:56 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -5,7 +5,7 @@ node{
   
   properties([[$class: 'BuildDiscarderProperty',
                 strategy: [$class: 'LogRotator', numToKeepStr: '10']],
-            pipelineTriggers([cron('* * * * *')]),
+            pipelineTriggers([cron('H/10 * * * *')]),
             parameters(
               [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
                string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
@@ -14,25 +14,7 @@ node{
                string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
             )
                 ])
-  //properties(
-    //[
-      //parameters(
-        //[string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
-        //string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
-        //string(name: 'jenkinsdslgitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote for Jenkins DSL repo'),
-        //string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
-        //string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
-        //)
-     //]
-  //)
   
-//  environment {
-//            gitremote = "${params.gitremote}"
-//            gitremote_training = "${params.gitremote_training}"
-//            jenkinsdslgitremote = "${params.jenkinsdslgitremote}"
-//            jenkinsdslgitbranch = "${params.jenkinsdslgitbranch}"
-//            jobtype = "${params.jobtype}"
-//  }
   ci = load 'nightly.groovy'
   ci.ciBranch()
 }
