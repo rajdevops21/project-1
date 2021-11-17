17f1d360cb4a4a5fe8166b958d6e47720cf397e2
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-17 08:21:53 -0500

    Update nightly-pipeline.groovy

--- nightly-pipeline.groovy
+++ nightly-pipeline.groovy
@@ -1,12 +1,20 @@
-#!groovy
+def nightly
 
-@Library('test-jenkins') _
-jenkinsCommon 'parent-nightly'
-
-triggers {
-        // Requires https://plugins.jenkins.io/parameterized-scheduler/
-        parameterizedCron('''
-            # pdsw-sonos-controller-player-s2 repo
-            H 23 * * *   %jobtype=parent-nightly
-        ''')
-    }
+node{
+  checkout scm
+  
+  properties([[$class: 'BuildDiscarderProperty',
+                strategy: [$class: 'LogRotator', numToKeepStr: '30']],
+            pipelineTriggers([cron('H/10 * * * *')]),
+            parameters(
+              [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
+               string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
+               string(name: 'jenkinsdslgitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote for Jenkins DSL repo'),
+               string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
+               string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
+            )
+                ])
+  
+  nightly = load 'nightly.groovy'
+  nightly.ciBranch()
+}
