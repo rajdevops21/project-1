e02b50748804188732833a2a30a80a5f61d30e73
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-17 08:24:27 -0500

    Update nightly-pipeline.groovy

--- nightly-pipeline.groovy
+++ nightly-pipeline.groovy
@@ -11,10 +11,10 @@ node{
                string(name: 'gitremote_training', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
                string(name: 'jenkinsdslgitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote for Jenkins DSL repo'),
                string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
-               string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
+               string(name: 'jobtype', defaultValue: 'nightly', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
             )
                 ])
   
-  nightly = load 'nightly.groovy'
-  nightly.ciBranch()
+  nightly = load 'common.groovy'
+  nightly.commoncode()
 }
