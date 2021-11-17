c5b8428b9e3ff00b4ec94be1318b8987bfb6080b
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-16 22:28:45 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -1,12 +1,13 @@
 def ci
-properties([[$class: 'BuildDiscarderProperty',
-                strategy: [$class: 'LogRotator', numToKeepStr: '10']],
-            pipelineTriggers([cron('H/1 * * * *')]),
-                ])
+
 node{
   checkout scm
   
-properties(
+  properties([[$class: 'BuildDiscarderProperty',
+                strategy: [$class: 'LogRotator', numToKeepStr: '10']],
+            pipelineTriggers([cron('H/1 * * * *')]),
+                ])
+  properties(
     [
       parameters(
         [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
@@ -15,7 +16,7 @@ properties(
         string(name: 'jenkinsdslgitbranch', defaultValue: 'main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
         string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
         )
-      ]
+     ]
   )
   
 //  environment {
