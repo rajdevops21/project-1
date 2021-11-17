98b6d3116e3b5f5a6822c6a980d94d4fce37016d
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-11-16 21:02:08 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -5,13 +5,17 @@ properties([[$class: 'BuildDiscarderProperty',
                 ])
 node{
   checkout scm
-  parameters {
-        string(name: 'gitremote', defaultValue: 'git@github.com:Sonos-Inc/pdsw-sonos-controller-player-s2.git', description: 'Git remote with product source code')
-        string(name: 'gitremote_training', defaultValue: 'git@github.com:Sonos-Inc/pdsw-sonos-controller-player-s2-training.git', description: 'Git remote with product source code')
-        string(name: 'jenkinsdslgitremote', defaultValue: 'git@github.com:Sonos-Inc/pdsw-jenkins-dsl.git', description: 'Git remote for Jenkins DSL repo')
-        string(name: 'jenkinsdslgitbranch', defaultValue: 'p4/main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from')
-        string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')
-  }
+  properties(
+    [
+      parameters(
+        [string(name: 'gitremote', defaultValue: 'git@github.com:Sonos-Inc/pdsw-sonos-controller-player-s2.git', description: 'Git remote with product source code'),
+        string(name: 'gitremote_training', defaultValue: 'git@github.com:Sonos-Inc/pdsw-sonos-controller-player-s2-training.git', description: 'Git remote with product source code'),
+        string(name: 'jenkinsdslgitremote', defaultValue: 'git@github.com:Sonos-Inc/pdsw-jenkins-dsl.git', description: 'Git remote for Jenkins DSL repo'),
+        string(name: 'jenkinsdslgitbranch', defaultValue: 'p4/main', description: 'Git branch for Jenkins DSL repo to pull dsl-branches and enabled-jobs.json from'),
+        string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly')]
+        )
+      ]
+  )
   environment {
             gitremote = "${params.gitremote}"
             gitremote_training = "${params.gitremote_training}"
