a6cf5f9ef05afc988f7fe584365e5fe628f28b70
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-01 05:55:26 -0500

    Update queued.py

--- queued.py
+++ queued.py
@@ -10,7 +10,7 @@ import urllib2
 
 UPSTREAM_JOB_NAME = os.getenv('JOB_NAME')
 UPSTREAM_BUILD_ID = os.getenv('BUILD_NUMBER')
-JENKINS_URL = os.getenv('JENKINS_URL').rstrip('/')
+JENKINS_URL = http://10.25.12.43:8080
 JENKINS_USER = os.getenv('JENKINS_USER') or os.getenv('JENKINS_USERNAME')
 JENKINS_TOKEN = os.getenv('JENKINS_API_TOKEN') or os.getenv('JENKINS_API_KEY')
 
