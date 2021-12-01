ff702e1d9fa3766dcf340df1a98a24d46658e016
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-01 06:23:39 -0500

    Update queued.py

--- queued.py
+++ queued.py
@@ -12,7 +12,7 @@ UPSTREAM_JOB_NAME = os.getenv('JOB_NAME')
 UPSTREAM_BUILD_ID = os.getenv('BUILD_NUMBER')
 JENKINS_URL = os.getenv('JENKINS_URL').rstrip('/')
 JENKINS_USER = os.getenv('JENKINS_USER') or os.getenv('JENKINS_USERNAME')
-JENKINS_TOKEN = os.getenv('JENKINS_API_TOKEN') or os.getenv('JENKINS_API_KEY')
+JENKINS_TOKEN = 'BocceBal1'
 
 ENCODED_TOKEN = base64.b64encode('%s:%s' % (JENKINS_USER, JENKINS_TOKEN)).decode('ascii')
 HEADERS = {
