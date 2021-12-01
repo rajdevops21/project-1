9b302c66971a29f527c7eb9ca0278f08eae5cb14
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-01 06:28:56 -0500

    Update queued.py

--- queued.py
+++ queued.py
@@ -10,9 +10,9 @@ import urllib2
 
 UPSTREAM_JOB_NAME = os.getenv('JOB_NAME')
 UPSTREAM_BUILD_ID = os.getenv('BUILD_NUMBER')
-JENKINS_URL = os.getenv('JENKINS_URL').rstrip('/')
+JENKINS_URL = "http://10.25.12.43:8080"
 JENKINS_USER = os.getenv('JENKINS_USER') or os.getenv('JENKINS_USERNAME')
-JENKINS_TOKEN = 'BocceBal1'
+JENKINS_TOKEN = "BocceBal1"
 
 ENCODED_TOKEN = base64.b64encode('%s:%s' % (JENKINS_USER, JENKINS_TOKEN)).decode('ascii')
 HEADERS = {
