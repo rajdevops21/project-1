5759c8599464fdb82faafd09c9a6d5e7c78e7628
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-21 21:40:38 -0500

    Create raj-log.groovy

---
+++ raj-log.groovy
@@ -0,0 +1,13 @@
+pipeline {
+    agent none
+    options { timestamps () }
+    stages {
+        stage('Example Build') {
+            steps {
+                sh '''#!/bin/bash
+                  chmod +x log.py
+                  python3 log.py
+            }
+        }
+     }
+}
