d44361a6085ca8ffbdab963c3f153c6206bea15c
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-21 20:33:00 -0500

    Update log.py

--- log.py
+++ log.py
@@ -2,5 +2,5 @@
 
 import logging
 
-logging.basicConfig(format='%(process)d-%(levelname)s-%(message)s')
-logging.warning('This message is a warning')
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.info('This message is to indicate that Admin has just logged in')
