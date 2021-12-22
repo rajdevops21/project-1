5161123ab45aaee20677f46f213db12452bceb6b
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-21 21:31:39 -0500

    Update log.py

--- log.py
+++ log.py
@@ -3,4 +3,5 @@
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-logging.info('This message is to indicate that Admin has just logged in')
+logging.info('You are logging this information')
+logging.info('This is python script')
