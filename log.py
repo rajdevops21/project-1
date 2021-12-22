88cbe6ca9fb67b6cf4303c31762fc1c0d57e099a
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-21 20:25:36 -0500

    Create log.py

---
+++ log.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+logging.warning('The Admin just logged out')
+logging.info('Mahes is a very taleneted guy')
