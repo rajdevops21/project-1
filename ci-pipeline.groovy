24e8d441ffc1dab744b14182bf7920d1269a9375
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2021-12-21 22:01:14 -0500

    Update ci-pipeline.groovy

--- ci-pipeline.groovy
+++ ci-pipeline.groovy
@@ -12,7 +12,7 @@ node{
               [string(name: 'gitremote', defaultValue: 'https://github.com/github/hackathons.git', description: 'Git remote with product source code'),
                string(name: 'jobtype', defaultValue: 'ci', description: 'Type of jobs to trigger: ci, parent-nightly, weekly-nightly'),]
             )
-                ])
+              ])
   
   ci = load 'common.groovy'
   ci.commoncode()
