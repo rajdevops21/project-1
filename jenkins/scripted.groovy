95cc2234be6de0c4c8a2b8d158c534876851fc89
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2022-01-07 12:52:54 -0500

    Update scripted.groovy

--- jenkins/scripted.groovy
+++ jenkins/scripted.groovy
@@ -1,3 +1,17 @@
-withGroovy {
-    
+node {
+    timestamps {
+        checkout scm
+        withGroovy {
+            import hudson.model.*
+            def jobName = 'DEV'
+            def q = Jenkins.instance.queue
+            q.items.findAll { it.task.name.contains(jobName) }.each { 
+                println("queued: " + it.task.name)
+            }
+            def running = Jenkins.instance.getView('All').getBuilds().findAll{ it.getResult().equals(null) && it.toString().contains(jobName) }
+            running.each {
+                println("running: " + it)
+            }
+        }
+    }
 }
