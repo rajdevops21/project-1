59aaaca51edd8251847d183e350432dcf488edcc
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2022-01-07 13:11:38 -0500

    Update scripted.groovy

--- jenkins/scripted.groovy
+++ jenkins/scripted.groovy
@@ -1,11 +1,11 @@
+import hudson.model.*
+import jenkins.model.Jenkins
 pipeline {
   agent any
   stages {
     stage ('Run the groovy script') {
       steps {
         script {
-            import hudson.model.*
-            import jenkins.model.Jenkins
             def jobName = 'main-player-matrix-pr'
             def q = Jenkins.instance.queue
             q.items.findAll { it.task.name.toLowerCase().contains(jobName) }.each { 
