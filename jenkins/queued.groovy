d05973e25ea01128546dfbfc506b9d6190a3aa3c
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2022-01-07 11:43:08 -0500

    Update queued.groovy

--- jenkins/queued.groovy
+++ jenkins/queued.groovy
@@ -1,12 +1,14 @@
 import hudson.model.*
 import jenkins.model.Jenkins
-def jobName = 'main-player-matrix-pr'
-def q = Jenkins.instance.queue
-q.items.findAll { it.task.name.toLowerCase().contains(jobName) }.each { 
- 
- println("queued: " + it.task.name)
+
+def commoncode(){
+ def jobName = 'main-player-matrix-pr'
+ def q = Jenkins.instance.queue
+ q.items.findAll { it.task.name.toLowerCase().contains(jobName) }.each { 
+  println("queued: " + it.task.name)
+ }
+ def running = Jenkins.instance.getView('All').getBuilds().findAll{ it.getResult().equals(null) && it.toString().contains(jobName) } running.each {
+  println("running: " + it)
+ }
 }
-def running = Jenkins.instance.getView('All').getBuilds().findAll{ it.getResult().equals(null) && it.toString().contains(jobName) }
-running.each {
- println("running: " + it)
-}
+return this
