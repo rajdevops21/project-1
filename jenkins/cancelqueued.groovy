13c00c3ab3963a90b932b1c0718d97853f8c5bd0
Rajkumar Devaraveni <90782047+rdevaravenisonos@users.noreply.github.com>
2022-01-07 18:13:11 -0500

    Update cancelqueued.groovy

--- jenkins/cancelqueued.groovy
+++ jenkins/cancelqueued.groovy
@@ -15,7 +15,6 @@ node {
         def q = Jenkins.instance.queue
         q.items.findAll { it.task.name.contains(jobName) }.each {
           println("queued: " + it.task.name)
-          q.cancel(it.task)
         }
         def running = Jenkins.instance.getView('All').getBuilds().findAll{ it.getResult().equals(null) && it.toString().contains(jobName) }
         running.each {
